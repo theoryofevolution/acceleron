3cdf4cc30bf43266c309a2205fd628bd5586f153
Yash Sharma <yash.sharma.now@gmail.com>
2023-08-04 21:25:27 -0700

    updated time

--- login.py
+++ login.py
@@ -28,51 +28,41 @@ def get_data():
         return pd.read_csv('leaderboard.csv')
 
 timer = """
-<!DOCTYPE html>
-<html>
-<head>
-    <style>
-    body {
-        font-family: sans-serif;
-        color: #31333F;
-        font-size: 20px; /* adjust size as necessary */
-        font-weight: bold;
-    }
-    </style>
 <script>
-function startTimer(targetTime, display) {
-    setInterval(function () {
-        var currentTime = new Date();
-        var remainingTime = targetTime - currentTime;
+function countdownTimer(endDate, display) {
+    var timer = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = endDate - now;
 
-        var hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+        days = days < 10 ? "0" + days : days;
         hours = hours < 10 ? "0" + hours : hours;
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = hours + ":" + minutes + ":" + seconds;
+        display.textContent = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-        if (remainingTime < 0) {
-            clearInterval(this);
-            display.textContent = "00:00:00";
+        if (distance < 0) {
+            clearInterval(timer);
+            display.textContent = "Time is up!";
         }
     }, 1000);
 }
 
 window.onload = function () {
-    var targetTime = new Date();
-    targetTime.setHours(17, 0, 0, 0); // Set target time to 5:00 PM (17:00)
+    var endDate = new Date("2023-08-30T17:00:00").getTime();  // Set your end date and time
     var display = document.querySelector('#time');
-    startTimer(targetTime, display);
+    countdownTimer(endDate, display);
 };
 </script>
+
 <body>
-  <div>Time left <span id="time">17:00:00</span></div>
+  <div style="font-family: sans-serif; font-size: 24px; font-weight: bold; color: #31333F;">Competition closes in <span id="time">00d 00h 00m 00s </span></div>
 </body>
-</html>
 """
 
 
@@ -105,29 +95,35 @@ if authentication_status:
             if uploaded_file.type == "text/csv":
                 df = pd.read_csv(uploaded_file)['LABEL']
         submitted = st.form_submit_button("Submit", type='primary')
-        current_hour = datetime.now().hour
-        current_minute = datetime.now().minute
-    if current_hour >= 17 and current_minute >= 0:
-        submitted = False
-        if st.button('The results are out!'):
-            switch_page('winners')
-    elif submitted:
-        if uploaded_file is None:
-            st.warning("You must submit a CSV file!")
-        else:
-            model_score = accuracy_score(ground_truth, df)
-            user_row = pd.Series([model_score, username], index=leaderboard.columns)
 
-            if username in leaderboard['Team'].values:
-                leaderboard.loc[leaderboard['Team'] == username, 'Score'] = model_score
-                leaderboard.loc[leaderboard['Team'] == username, 'Team'] = username
+        # Get current date and time
+        current_time = datetime.now()
+        current_hour = current_time.hour
+        current_minute = current_time.minute
+        current_day = current_time.day
+
+        # Check if it's 10 pm and the 15th day of the month
+        if current_hour >= 17 and current_minute >= 0 and current_day == 30:
+            submitted = False
+            if st.button('The results are out!'):
+                switch_page('winners')
+        elif submitted:
+            if uploaded_file is None:
+                st.warning("You must submit a CSV file!")
             else:
-                leaderboard = leaderboard.append(user_row, ignore_index=True)
+                model_score = accuracy_score(ground_truth, df)
+                user_row = pd.Series([model_score, username], index=leaderboard.columns)
+                if username in leaderboard['Team'].values:
+                    leaderboard.loc[leaderboard['Team'] == username, 'Score'] = model_score
 
-            # Sorting the dataframe by Score in descending order
-            leaderboard = leaderboard.sort_values(by='Score', ascending=False).reset_index(drop=True)
-            leaderboard.to_csv('leaderboard.csv', index=False)
-            st.table(leaderboard)
+                    leaderboard.loc[leaderboard['Team'] == username, 'Team'] = username
+                else:
+                    leaderboard = leaderboard.append(user_row, ignore_index=True)
+
+                # Sorting the dataframe by Score in descending order
+                leaderboard = leaderboard.sort_values(by='Score', ascending=False).reset_index(drop=True)
+                leaderboard.to_csv('leaderboard.csv', index=False)
+                st.table(leaderboard)
 
 elif authentication_status == False:
     st.error('Username/password is incorrect')
